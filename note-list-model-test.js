8138b6b4671c3d28046a1e0ddc208fb9429f86ae
Léa R <lea.rosamond@gmail.com>
2018-01-08 17:20:27 +0000

    adds an AddNoteToList Method

--- note-list-model-test.js
+++ note-list-model-test.js
@@ -1,6 +1,16 @@
-function createANote(){
+function createAList(){
   var list = new List()
   assert.isTrue(list.notes.length === 0, "Note list should be an empty array");
   };
 
-createANote();
+createAList();
+
+function AddNoteToList() {
+  var list = new List()
+  var note = new Note("Lea's note")
+  list.AddNoteToList(note);
+  assert.isTrue(list.notes.length === 1, "A note was added to the list")
+
+};
+
+ AddNoteToList();
--- note-list-model.js
+++ note-list-model.js
@@ -1,6 +1,11 @@
 (function(exports) {
   function List() {
     this.notes = [];
-  }
+  };
+  // var note = new Note("note")
+    List.prototype.AddNoteToList = function (note) {
+    this.notes.push(note);
+  };
+  
   exports.List = List;
 })(this);
