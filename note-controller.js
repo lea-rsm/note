a832b83727b3369f7bd08cdb763207e68f3dd118
Léa R <lea.rosamond@gmail.com>
2018-01-09 15:28:10 +0000

    try of the note-controller consol logging

---
+++ note-controller.js
@@ -0,0 +1,9 @@
+console.log('test1')
+window.onLoad = function(){
+  console.log('test2')
+  var location = document.getElementById("app");
+  console.log('test3')
+};
+console.log('test4')
+console.log(location)
+console.log('test5')
