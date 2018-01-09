d29431a1e173537da033e3534e9aa68a8f4e2e8e
tom <tomallpress@hotmail.com>
2018-01-09 16:32:55 +0000

    able to display lists on server page

--- note-controller.js
+++ note-controller.js
@@ -1,3 +1,12 @@
 window.onload = function(){
   console.log(document.getElementById('app'));
+  list = new List;
+  list.AddNoteToList("Appear to the screen please");
+  list.AddNoteToList("Appearrrrrr");
+  list.AddNoteToList("Appear to the screen ddddd");
+  list.AddNoteToList("Appear to the screen ddddd");
+  list.AddNoteToList("Appear  ddddd");
+  list.AddNoteToList("Appear to f screen ddddd");
+  printer = new NoteViewer(list);
+  printer.viewNotes();
 }
